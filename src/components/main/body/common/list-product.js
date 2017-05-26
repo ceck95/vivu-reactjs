e3d1fd2550fcb9ec9592efe13b2740154d9993ac
nhutdev <tranvannhut4495@gmail.com>
2017-05-26 10:48:35 +0700

    fix ui list product

--- src/components/main/body/common/list-product.js
+++ src/components/main/body/common/list-product.js
@@ -1,28 +1,20 @@
 /**
- * @Author: Tran Van Nhut <nhutdev>
- * @Date:   2017-02-18T13:50:18+07:00
- * @Email:  tranvannhut4495@gmail.com
+* @Author: Tran Van Nhut <nhutdev>
+* @Date:   2017-02-18T13:50:18+07:00
+* @Email:  tranvannhut4495@gmail.com
  * @Last modified by:   nhutdev
  * @Last modified time: 2017-03-24T08:37:47+07:00
- */
+*/
 
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 
 //map props
-import {
-  connect
-} from 'react-redux';
-import {
-  bindActionCreators
-} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import actions from '../../../../actions/index';
 import ReactBase from 'react-base';
 
-import {
-  Link
-} from 'react-router';
+import { Link } from 'react-router';
 
 //config
 import config from '../../../../config/index';
@@ -88,15 +80,15 @@ class ListProduct extends Component {
 
   addToCart(e) {
     if (!e.isProductColor)
-      if (e.productColor ?
-        e.productColor.id :
-        false) {
+      if (e.productColor
+          ? e.productColor.id
+          : false) {
         this.props.actions.setQuoteItem({
           productId: e.id,
           selectedProductColorId: e.productColor.id,
           quantity: 1
         }, this.props.dataQuoteCart, this.props.dataQuote);
-      }
+    }
 
   }
   getUrlKey(e) {
@@ -125,70 +117,40 @@ class ListProduct extends Component {
         // this.state.dataItemCategory.categories.forEach(a => {
         // if (e.categoryId === a.id) {
         let productKey = this.getUrlKey(e);
-        listElementProduct.push( <
-          div key = {
-            i
-          }
-          className = "col-sm-6 col-md-4"
-          style = {
-            {
-              height: this.state.heightElement
-            }
-          } >
-          <
-          div ref = "tabsItems"
-          className = "tabs_items" >
-          <
-          Link className = "link-img"
-          to = {
-            `/${productKey}`
-          } >
-          <
-          Image linkImage = {
-            e.imagePath
-          }
-          /> < /
-          Link > <
-          Link to = {
-            `/${productKey}`
-          }
-          className = "link-text" > {
-            e.name
-          } <
-          /Link> <
-          p className = "price-current bold" > {
-            utility.formatCurrency(e.basePrice)
-          } <
-          /p> <
-          button className = "add-to-cart"
-          onClick = {
-            this.addToCart.bind(this, e)
-          } >
-          <
-          i className = "fa fa-cart-plus"
-          aria - hidden = "true" > < /i>Thêm vào giỏ hàng</button >
-          <
-          /div> < /
-          div >
+        listElementProduct.push(
+          <div key={ i } className="col-sm-6 col-md-4" style={ { height: this.state.heightElement } }>
+            <div ref="tabsItems" className="tabs_items">
+              <Link className="link-img" to={ `/${productKey}` }>
+              <Image linkImage={ e.imagePath } />
+              </Link>
+              <Link to={ `/${productKey}` } className="link-text">
+              { e.name }
+              </Link>
+              <p className="price-current bold">
+                { utility.formatCurrency(e.basePrice) }
+              </p>
+              <button className="add-to-cart" onClick={ this.addToCart.bind(this, e) }>
+                <i className="fa fa-cart-plus" aria-hidden="true"></i>Thêm vào giỏ hàng</button>
+            </div>
+          </div>
         )
-        // }
-        // });
+      // }
+      // });
       });
     }
 
     if (listElementProduct.length > 0) {
-      return ( <
-        div > {
-          listElementProduct
-        } <
-        /div>
+      return (
+        <div>
+          { listElementProduct }
+        </div>
       )
     }
 
-    return ( <
-      div >
-      Loading Product <
-      /div>
+    return (
+      <div>
+        Loading Product
+      </div>
     )
 
   }
